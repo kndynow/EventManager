12b260d83df4ad8bd6acea67d161cf4e3a59064e
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-03-13 16:12:10 +0100

    Set up the connection to the MongoDb. Uses environment variable for the connection string. Currently supports only the Create Event API call.

---
+++ EventManager.Core/Data/EventRepository.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Driver;
+using EventManager.Core.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventManager.Core.Data
+{
+    public class EventRepository
+    {
+        private readonly IMongoCollection<Event> _events;
+
+        public EventRepository(IOptions<MongoDbSettings> options)
+        {
+            var settings = options.Value;
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _events = database.GetCollection<Event>("Events");
+        }
+
+        public async Task CreateAsync(Event newEvent)
+        {
+            await _events.InsertOneAsync(newEvent);
+        }
+    }
+}
