c64338f697c748156891f5b3e44be683c1b40dca
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-03-14 11:27:05 +0100

    Created the shell for AuthService and AuthRepository. Started working on the Register-method in the service and repository.

---
+++ EventManager.Core/Data/AuthRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventManager.Core.Models;
+
+
+
+namespace EventManager.Core.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        // Handles interactions with the MongoDB database for user-related data.
+        // It connects to the MongoDB server using the connection string and database name from the MongoDbSettings-class.
+
+        private readonly IMongoCollection<User> _users;
+
+        public AuthRepository(IOptions<MongoDbSettings> options)
+        {
+            var settings = options.Value;
+            var client = new MongoClient(settings.ConnectionString);
+            var database = client.GetDatabase(settings.DatabaseName);
+            _users = database.GetCollection<User>("Users");
+        }
+
+
+
+
+    }
+}
--- EventManager.Core/Services/AuthService.cs
+++ EventManager.Core/Services/AuthService.cs
@@ -1,49 +1,49 @@
-using EventManager.Core.Data;
-using EventManager.Core.Models;
+//using EventManager.Core.Data;
+//using EventManager.Core.Models;
 
-namespace EventManager.Core.Services;
+//namespace EventManager.Core.Services;
 
-public interface IAuthService
-{
-    User? Login(string username, string password);
-    User? Register(string username, string password);
-}
+//public interface IAuthService
+//{
+//    User? Login(string username, string password);
+//    User? Register(string username, string password);
+//}
 
-public class AuthService : IAuthService
-{
-    private readonly IDatabase _database;
+//public class AuthService : IAuthService
+//{
+//    private readonly IAuthRepository _authRepository;
 
-    public AuthService(IDatabase database)
-    {
-        _database = database;
-    }
+//    public AuthService(IAuthRepository authRepository)
+//    {
+//        _authRepository = authRepository;
+//    }
 
-    public User? Login(string username, string password)
-    {
-        var user = _database.Users.FirstOrDefault(u => u.Username == username);
-        if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
-        {
-            return null;
-        }
+//    public User? Login(string username, string password)
+//    {
+//        var user = _database.Users.FirstOrDefault(u => u.Username == username);
+//        if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.PasswordHash))
+//        {
+//            return null;
+//        }
 
-        return new User(user.Username, user.Role);
-    }
+//        return new User(user.Username, user.Role);
+//    }
 
-    //TODO: Refractor for better readability
-    // Maybe create a create a IValidator / Validator?
-    public User? Register(string username, string password)
-    {
-        if (
-            _database.Users.Any(u => u.Username == username || string.IsNullOrWhiteSpace(username))
-            || string.IsNullOrWhiteSpace(password)
-        )
-        {
-            return null;
-        }
+//    //TODO: Refractor for better readability
+//    // Maybe create a create a IValidator / Validator?
+//    public User? Register(string username, string password)
+//    {
+//        if (
+//            _database.Users.Any(u => u.Username == username || string.IsNullOrWhiteSpace(username))
+//            || string.IsNullOrWhiteSpace(password)
+//        )
+//        {
+//            return null;
+//        }
 
-        var user = new User(username, BCrypt.Net.BCrypt.HashPassword(password));
+//        var user = new User(username, BCrypt.Net.BCrypt.HashPassword(password));
 
-        _database.Users.Add(user);
-        return user;
-    }
-}
+//        _database.Users.Add(user);
+//        return user;
+//    }
+//}
