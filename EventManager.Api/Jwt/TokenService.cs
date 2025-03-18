c601da595e4ffc495dc887299ba2d5d2d4df3af2
cccowboy <joakim.andersson.syne24lin@edu.tucsweden.se>
2025-03-18 14:06:59 +0100

    Develop (#62)
    Co-authored-by: Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
    Co-authored-by: Edvard <edde113@hotmail.com>

---
+++ EventManager.Api/Jwt/TokenService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using static EventManager.Api.Jwt.TokenService;
+
+namespace EventManager.Api.Jwt
+{
+    public class TokenService : ITokenService
+    {
+        public interface ITokenService
+        {
+            public string GenerateToken(string username, string role);
+        }
+
+        private readonly IConfiguration _config; 
+
+        public TokenService (IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateToken(string username, string role)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]!);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _config["JwtSettings:Issuer"],
+                Audience = _config["JwtSettings:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
--- EventManager.Client/Configuration/ApiConfig.cs
+++
@@ -1,6 +0,0 @@
-namespace EventManager.Client.Configuration;
-
-public class ApiConfig
-{
-    public string BaseUrl { get; set; } = string.Empty;
-}
