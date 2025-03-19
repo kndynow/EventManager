6b1146a2c8aaa87b9c5504ff5e4bcdf6b965ef82
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-03-19 15:49:26 +0100

    Created the base structure for the blazor SignUp-page.

---
+++ EventManager.Client/Models/SignUp/SignUpModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.Client.Models.SignUp
+{
+    public class SignUpModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required")]
+        public string? Username { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+        public string? Password { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Repeat password for verification")]
+        public string? RepeatPassword { get; set; }
+    }
+}
