3343f6c7addd5ddf030c2f0b74f4799a524c1f25
Edvard Andersson <edvard.andersson.syne24lin@edu.tucsweden.se>
2025-03-26 21:06:59 +0100

    Added Get and UpdatePartial endpoints for the user management. Started working on admin function being able to change roles but didn't have time to finish.

---
+++ EventManager.Api/Endpoints/UserManagement/Get.cs
@@ -0,0 +1,30 @@
+﻿using EventManager.Api.Models;
+using Mapster;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.Api.Endpoints.UserManagement;
+public class GetUser : IEndpoint
+{
+    // Mapping
+    public static void MapEndpoint(IEndpointRouteBuilder app) =>
+        app.MapGet("/users/{id}", Handle).WithSummary("Get user");
+
+    public record Request(string Id);
+
+    //Logic
+    private static async Task<IResult> Handle(
+        [AsParameters] Request request,
+        IUserService userService
+    )
+    {
+        try
+        {
+            var ev = await userService.GetUserByIdAsync(request.Id);
+            return Results.Ok(ev);
+        }
+        catch (KeyNotFoundException)
+        {
+            return Results.NotFound($"User with id {request.Id} was not found.");
+        }
+    }
+}
